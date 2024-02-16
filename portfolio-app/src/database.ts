eee9374d244a1f4cc34cb556eec6e08bd8dc4609
jvpf <jvictorpf@outlook.com>
2024-02-16 16:25:01 -0300

    feat: project structure

---
+++ portfolio-app/src/database.ts
@@ -0,0 +1,111 @@
+// name: "",
+// description:"",
+// technologies:[],
+// github:"",
+// link: 
+
+export const projects = 
+[
+{
+    name: "Criação site institucional genérico",
+    description:"Criação site institucional genérico com o objetivo de exercitar conceitos aprendidos sobre HTML e CSS.",
+    technologies:["HTML", "CSS"],
+    github:"https://github.com/joaovpf1/m2-criando-site-institucional-generico-joaovpf1",
+    link:"https://kenzie-academy-brasil-developers.github.io/m2-criando-site-institucional-generico-joaovpf1/",
+},
+{
+    name: "Estilizando rede social",
+    description:"API homepage de uma rede social básica, com o intuito de exercitar HTML, CSS e Javascript",
+    technologies:["HTML", "CSS", "JavaScript"],
+    github:"https://github.com/joaovpf1/entrega-criando-rede-social-jvictorpf1",
+    link: "",
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+{
+    name: "",
+    description:"",
+    technologies:[],
+    github:"",
+    link: 
+    
+},
+]
