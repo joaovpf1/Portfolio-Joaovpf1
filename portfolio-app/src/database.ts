95d66412ef3bb1e1c6718787e68ec6d665335dad
jvpf <jvictorpf@outlook.com>
2024-02-21 15:56:13 -0300

    feat: base structure

--- portfolio-app/src/database.ts
+++ portfolio-app/src/database.ts
@@ -10,102 +10,171 @@ export const projects =
     name: "Criação site institucional genérico",
     description:"Criação site institucional genérico com o objetivo de exercitar conceitos aprendidos sobre HTML e CSS.",
     technologies:["HTML", "CSS"],
-    github:"https://github.com/joaovpf1/m2-criando-site-institucional-generico-joaovpf1",
-    link:"https://kenzie-academy-brasil-developers.github.io/m2-criando-site-institucional-generico-joaovpf1/",
+    github:"https://github.com/joaovpf1/criando-site-institucional-generico-joaovpf1",
+    link:"https://joaovpf1.github.io/criando-site-institucional-generico-joaovpf1/",
 },
 {
     name: "Estilizando rede social",
     description:"API homepage de uma rede social básica, com o intuito de exercitar HTML, CSS e Javascript",
     technologies:["HTML", "CSS", "JavaScript"],
-    github:"https://github.com/joaovpf1/entrega-criando-rede-social-jvictorpf1",
+    github:"https://github.com/joaovpf1/criando-rede-social-joaovpf1",
+    link: "https://joaovpf1.github.io/criando-rede-social-joaovpf1/",
+},
+{
+    name: "Control finance",
+    description:"Sistema básico de controle de entrada e saídas financeiras, utilizando JS, CSS e HTML como base.",
+    technologies:["HTML", "CSS", "JavaScript"],
+    github:"https://github.com/joaovpf1/api-control-finance-joaovpf1",
+    link: "https://joaovpf1.github.io/api-control-finance-joaovpf1/",
+},
+{
+    name: "Open Music",
+    description:"Api de uma loja de discos musicais com filtros de gênero e preço, além de apresentar modo noturno, tecnologias usadas: CSS, HTML e JS.",
+    technologies:["HTML", "CSS", "JavaScript"],
+    github:"https://github.com/joaovpf1/open-music-joaovpf1",
+    link: "https://joaovpf1.github.io/open-music-joaovpf1/",
+    
+},
+{
+    name: "Pet info",
+    description:"Api de rede social pet, com o intuito de informar sobre pets e trocar informações sobre pets, foi usado JS, CSS e HTML.",
+    technologies:["HTML", "CSS", "JavaScript"],
+    github:"https://github.com/joaovpf1/pet-info-joaovpf1",
+    link: "https://joaovpf1.github.io/pet-info-joaovpf1/",
+    
+},
+{
+    name: "Portfolio template",
+    description:"Projeto feito em React com o intuito de estudar a tecnologia, com foco na estilização. Tecnologias usadas: react, css, html, js.",
+    technologies:["HTML", "CSS", "JavaScript", "React"],
+    github:"https://github.com/joaovpf1/portfolio-base-template-joaovpf1",
+    link: "https://m3-s1-entrega-portfolio-template-joaovpf1.vercel.app",
+    
+},
+{
+    name: "Nu kenzie",
+    description:"Sistema básico de controle financeiro, utilizando React, JS, SASS e HTML como base.",
+    technologies:["HTML", "SASS", "JavaScript", "React"],
+    github:"https://github.com/joaovpf1/nu-kenize-joaovpf1",
+    link: "https://react-entrega-s1-template-nu-kenzie-joaovpf1.vercel.app",
+    
+},
+{
+    name: "Kenzie burguer",
+    description:"Template de uma hamburgueria com opções de adicionar produtos ao seu carrinho, ver o preço, excluir produtos, filtrar produtos. Tecnologias utilizadas:  React, SASS, HTML, JS.",
+    technologies:["HTML", "SASS", "JavaScript", "React"],
+    github:"https://github.com/joaovpf1/kenzie-burguer-joaovpf1",
+    link: "https://react-entrega-s3-template-hamburgueria-joaovpf1.vercel.app",
+    
+},
+{
+    name: "Kenzie hub",
+    description:"Api para cadastro de tecnologias estudadas, feito com React, HTML, SASS e JS, CRUD feito com js assíncrono.",
+    technologies:["HTML", "SASS", "JavaScript", "React"],
+    github:"https://github.com/joaovpf1/kenzie-hub-joaovpf1",
+    link: "https://react-entrega-kenzie-hub-joaovpf1.vercel.app/register",
+    
+},
+{
+    name: "Kenzie feed",
+    description:"Api de notícias feita em grupo, com tela inicial, foco em notícia especifíca, crud de notícias, login, auto login. Tecnologias usadas: React, JS, SASS, HTML, Git e Gitflow. A API foi feia por 5 pessoas, tem 287 commits e 93 pul requests. Obs: o acesso ao link do github dessa aplicação é privado.",
+    technologies:["HTML", "SASS", "JavaScript", "React"],
+    github:"",
+    link: "https://kenzie-feed-grupo-2.vercel.app/"
+    
+},
+{
+    name: "Mercado autônomo",
+    description:"API utilizada para organização de estoque de um mercado autônomo, feito com typescript. obs: há apenas dois tipos de produtos, limpeza e comida.",
+    technologies:["TypeScript"],
+    github:"https://github.com/joaovpf1/mercado-autonomo-joaovpf1",
+    link: "" ,
+},
+{
+    name: "Locadora de filmes",
+    description:"API para gerenciamento de coleção de filmes para uma plataforma de streaming. Feito com typescript, sql e postgresql.",
+    technologies:["TypeScript", "SQL", "PostgreSQL"],
+    github:"https://github.com/joaovpf1/locadora-filmes-joaovpf1",
+    link: "",
+    
+},
+{
+    name: "Kenzievelopers",
+    description:"API REST de gerenciamento de desenvolvedores e projetos. Feito com typescript, sql e postgresql.",
+    technologies:["TypeScript", "SQL", "PostgreSQL"],
+    github:"https://github.com/joaovpf1/kenzievelopers-joaovpf1",
+    link: "",
+    
+},
+{
+    name: "MVP user control",
+    description:"MVP de uma API que faz o controle de usuários e cursos em que serão matriculados. Feito com typescript, sql e postgresql.",
+    technologies:["TypeScript", "SQL", "PostgreSQL", "Zod"],
+    github:"https://github.com/joaovpf1/mvp-user-control-joaovpf1",
+    link: ""
+    
+},
+{
+    name: "Locadora de filmes typeorm",
+    description:"API para gerenciamento de coleção de filmes para uma plataforma de streaming. Feito com typescript, sql, postgresql, zod e typeorm.",
+    technologies:["TypeScript", "SQL", "PostgreSQL", "TypeOrm", "Zod"],
+    github:"https://github.com/joaovpf1/locadora-filmes-typeorm-joaovpf1",
     link: "",
 },
 {
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
+    name: "Kimoveis",
+    description:"API de gerenciamento para imobiliária, com possibilidade de realizar cadastros de imóveis, usuários, realizar agendamentos e consultar horários de visitas disponíveis.",
+    technologies:["TypeScript", "SQL", "PostgreSQL", "TypeOrm", "Zod"],
+    github:"https://github.com/joaovpf1/kimoveirs-joaovpf1",
+    link: "",
 },
 {
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
+    name: "Kiosque",
+    description:"API para gerenciamento de um quiosque de comida, verificando produtos em menu, listar alimentos por categoria, calcula gastos de uma mesa e mais.",
+    technologies:["Python"],
+    github:"https://github.com/joaovpf1/kiosque-joaovpf1",
+    link: "",
 },
 {
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
+    name: "Copa do mundo",
+    description:"API para organização de um campeonato de futebol, com diferentes times, onde cada time representa uma seleção.",
+    technologies:["Python", "Django"],
+    github:"https://github.com/joaovpf1/copa-do-mundo-joaovpf1",
+    link: "",
 },
 {
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
+    name: "Petcare",
+    description:"Sistema para petshops com o intuito de melhorar o controle e organização das informações dos clientes.",
+    technologies:["Python", "Django"],
+    github:"https://github.com/joaovpf1/pet-kare-joaovpf1",
+    link: "",
 },
 {
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
+    name: "Kenzie buster",
+    description:"API para gestão de venda de filmes, objetivo de gerenciar usuários, filmes e compras.",
+    technologies:["Python", "Django", "Django Rest Framework", "JWT"],
+    github:"https://github.com/joaovpf1/kenzie-buster-joaovpf1",
+    link: "",
 },
 {
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
+    name: "Bandkamp",
+    description:"Refatoração de um projeto relacionado a música, projeto foi desenvolvido com django, apiview, serializer e sqlite3. A refatoração necessita os conceitos de generic view, model serializer e postgresql",
+    technologies:["Python", "Django", "Django Rest Framework", "JWT", "PostgreSql"],
+    github:"https://github.com/joaovpf1/bandkamp-joaovpf1",
+    link: "",
 },
 {
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
+    name: "Kanvas",
+    description:"API Rest para gerenciamento de cursos e aulas de uma escola de modalidade EAD.",
+    technologies:["Python", "Django", "Django Rest Framework", "JWT", "Swagger"],
+    github:"https://github.com/joaovpf1/kanvas-joaovpf1",
+    link: "",
 },
 {
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
-},
-{
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
-},
-{
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
-},
-{
-    name: "",
-    description:"",
-    technologies:[],
-    github:"",
-    link: 
-    
+    name: "Gerenciamento de contatos fullstack",
+    description:"Aplicação fullstack para gerenciamento de contatos, com possibilidades de cadastro de conta, criação, listagem, edição e exclusãodos contatos.",
+    technologies:["Javascript", "React", "SCSS", "HTML", "PostgreSQL"],
+    github:"https://github.com/joaovpf1/gerenciador-contatos-fullstack-joaovpf1",
+    link: "",
 },
 ]
