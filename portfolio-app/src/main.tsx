2a7e7d0a0af047356aba73bf6a648c9dff7dacf9
jvpf <jvictorpf@outlook.com>
2024-02-29 08:54:40 -0300

    feat: hotfix index script and add icon

--- portfolio-app/src/main.tsx
+++ portfolio-app/src/main.tsx
@@ -7,7 +7,9 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App /> 
+      {/* <RoutesMain> */}
+        <App /> 
+      {/* </RoutesMain> */}
     </BrowserRouter>
   </React.StrictMode>,
 )
